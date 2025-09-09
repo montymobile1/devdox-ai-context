92aeda36a28cb678a82385decbf322a8adf41733
Mohamad Ali Jaafar <mohamadali.jaafar@montyholding.com>
2025-09-09 13:16:26 +0300

    refactor: Refactored the email models
    Description:
    I discovered that Pydantic V2, and latest Java way of defining Optiona does not work well so I replaced (Type | None) with Optional[Type].
    
    Also, I should have used `default_factory` instead of `default` for Lists.

--- app/infrastructure/mailing_service/models.py
+++ app/infrastructure/mailing_service/models.py
@@ -1,16 +1,16 @@
-from typing import Annotated, Any, Mapping
+from typing import Annotated, Any, Mapping, Optional
 
 from pydantic import BaseModel, EmailStr, Field, StringConstraints
 
 NonBlankStr = Annotated[str, StringConstraints(strip_whitespace=True, min_length=1)]
 
 class EmailEnvelope(BaseModel):
-	subject: NonBlankStr = Field(..., description="The email’s title (what shows in the inbox). Keep it short and human (avoid ALL CAPS/spammy words).")
-	recipients: list[EmailStr] = Field(..., min_length=1, description="A list of “To” addresses (the main people getting the email).")
-	cc: list[EmailStr] | None = Field(None, description="Carbon copy recipients")
-	bcc: list[EmailStr] | None = Field(None,  description="Blind carbon copy recipients")
-	reply_to: list[EmailStr] | None = Field(None, description="If someone clicks “Reply”, messages go here instead of the “From” address. Useful when sending from a no-reply address but wanting replies to support@…")
-	headers: Mapping[str, NonBlankStr] | None = Field(None, description="Custom metadata for filters/automation (machines more than humans). (e.g., 'X-Campaign-ID': 'llm-summary-2025-08'")
+	subject: NonBlankStr = Field(default=..., description="The email’s title (what shows in the inbox). Keep it short and human (avoid ALL CAPS/spammy words).")
+	recipients: list[EmailStr] = Field(default=..., min_length=1, description="A list of “To” addresses (the main people getting the email).")
+	cc: Optional[list[EmailStr]] = Field(default_factory=list, description="Carbon copy recipients")
+	bcc: Optional[list[EmailStr]] = Field(default_factory=list,  description="Blind carbon copy recipients")
+	reply_to: Optional[list[EmailStr]] = Field(default_factory=list, description="If someone clicks “Reply”, messages go here instead of the “From” address. Useful when sending from a no-reply address but wanting replies to support@…")
+	headers: Optional[Mapping[str, NonBlankStr]] = Field(default=None, description="Custom metadata for filters/automation (machines more than humans). (e.g., 'X-Campaign-ID': 'llm-summary-2025-08'")
 
 
 class OutgoingTemplatedContextEmail(EmailEnvelope):
@@ -19,26 +19,26 @@ class OutgoingTemplatedContextEmail(EmailEnvelope):
 	)
 
 class OutgoingHtmlEmail(EmailEnvelope):
-	html_body: NonBlankStr = Field(..., description="Raw HTML body of the email")
-	text_fallback: NonBlankStr | None = Field(
-		None, description="Optional text-only multipart fallback if HTML is not supported"
+	html_body: NonBlankStr = Field(default=..., description="Raw HTML body of the email")
+	text_fallback: Optional[NonBlankStr] = Field(
+		default=None, description="Optional text-only multipart fallback if HTML is not supported"
 	)
 
 class OutgoingTextEmail(EmailEnvelope):
-	text_body: NonBlankStr = Field(..., description="Raw Plain text body of the email")
+	text_body: NonBlankStr = Field(default=..., description="Raw Plain text body of the email")
 
 class OutgoingTemplatedHTMLEmail(OutgoingTemplatedContextEmail):
 	
 	html_template: NonBlankStr = Field(
-		..., description="Name of the HTML template file name (e.g., analysis_summary.html)"
+		default=..., description="Name of the HTML template file name (e.g., analysis_summary.html)"
 	)
 	
-	plain_template_fallback: NonBlankStr | None = Field(
-		None, description="Optional text-only multipart fallback template file name if HTML is not supported"
+	plain_template_fallback: Optional[NonBlankStr] = Field(
+		default=None, description="Optional text-only multipart fallback template file name if HTML is not supported"
 	)
 
 class OutgoingTemplatedTextEmail(OutgoingTemplatedContextEmail):
 	plain_template: NonBlankStr = Field(
-		..., description="Name of the plain text template file name (e.g., analysis_summary.txt)"
+		default=..., description="Name of the plain text template file name (e.g., analysis_summary.txt)"
 	)
 
